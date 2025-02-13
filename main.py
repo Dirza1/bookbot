3ece9471a7c5eefb4f6545ac552f549e6b1755e0
Dirza1 <jasper.olthof@xs4all.nl>
2025-02-13 15:21:23 +0100

    Addded word count feature

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main ():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print (file_contents)
+        word_count = count_words(file_contents)
+        print (word_count)
+
+def count_words (book):
+    words = book.split()
+    count = len(words)
+    return count
 
 main()
